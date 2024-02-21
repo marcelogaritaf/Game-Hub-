4875fe67bbe7ecbcce2441053aa6da3fdbe40d1d
Marcelo Garita <mgaritaf@outlook.com>
2024-02-21 17:10:30 -0600

    created custom hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient, { AxiosError, CanceledError } from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-interface ApiGames {
-  id: number;
-  name: string;
-}
-interface FecthGames {
-  count: number;
-  // next:string,
-  // previous:string,
-  results: ApiGames[];
-}
+import useGames from "../hooks/useGames";
+
 const GameGrid = () => {
-  const [games, setGames] = useState<ApiGames[]>([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    const fectGames = async () => {
-      try {
-        const res = await apiClient.get<FecthGames>("/games");
-        setGames(res.data.results);
-      } catch (error) {
-        {
-          if (error instanceof CanceledError) return;
-          setError((error as AxiosError).message);
-        }
-      }
-    };
-    fectGames();
-  }, []);
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import apiClient, { AxiosError, CanceledError } from "../services/api-client";
+interface ApiGames {
+    id: number;
+    name: string;
+  }
+  interface FecthGames {
+    count: number;
+    // next:string,
+    // previous:string,
+    results: ApiGames[];
+  } 
+const useGames=()=>{
+    const [games, setGames] = useState<ApiGames[]>([]);
+    const [error, setError] = useState("");
+  useEffect(() => {
+    const controller= new AbortController
+    const fectGames = async () => {
+      try {
+        const res = await apiClient.get<FecthGames>("/games",{signal:controller.signal});
+        setGames(res.data.results);
+      } catch (error) {
+        {
+          if (error instanceof CanceledError) return;
+          setError((error as AxiosError).message);
+        }
+      }
+      
+    };
+    fectGames();
+    return () => controller.abort()
+  }, []);
+  return {games, error}
+}
+export default useGames
