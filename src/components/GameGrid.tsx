d5254a0df0ab52fe54c84200290ae8285a4feff8
Marcelo Garita <mgaritaf@outlook.com>
2024-02-26 16:50:56 -0600

    displaying img genres, fixing layout

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,13 +6,13 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeleton = [1, 2, 3, 4, 5, 6];
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 2, xl: 3 }}
-        padding={10}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        paddingX={10}
         spacing={5}
         justifyItems={"center"}
       >
