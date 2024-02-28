dcd67c9c710ffe17fe4804e6b32447b0a54f0213
Marcelo Garita <mgaritaf@outlook.com>
2024-02-28 15:09:03 -0600

    no image solving

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,8 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
 // metodo para poder optimizar el size de la imagen 
 const getCroppedImageUrl = (url:string)=>{
     //si el url es falso retorne un valor vacio
-    if (!url)return ''
+    if (!url)return noImage
     const target= 'media/'
     const index=url.indexOf(target)+target.length
    return url.slice(0,index)+ 'crop/600/400/' + url.slice(index)
