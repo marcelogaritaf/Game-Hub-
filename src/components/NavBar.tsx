3559b232986a71987ba00fc7241fc917c4a2b728
Marcelo Garita <mgaritaf@outlook.com>
2024-02-28 15:37:26 -0600

    input games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,18 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInputGames from "./SearchInputGames";
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding="12px">
+    <HStack padding="12px">
       <Image src={logo} boxSize="60px" />
+      <SearchInputGames />
       <ColorModeSwitch />
     </HStack>
   );
 };
 
 export default NavBar;
+/**
+ * por motivo de que ya hay tres componentes el justifyContent={"space-between"}
+ */
