87b5ef6bf9e1bf9efaf94e6113a61e4c6e490cfe
Marcelo Garita <mgaritaf@outlook.com>
2024-03-18 18:35:10 -0600

    implementing react-query in usePlatfomrs and platformSelector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -16,7 +16,7 @@ const PlatformSelector = ({ onselectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onselectPlatform(platform)}
             key={platform.id}
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,26 @@
-import useData from "./useData"
+import { useQuery } from "@tanstack/react-query"
+import useData, { FecthResponse } from "./useData"
+import apiClient from "../services/api-client"
 
 interface Platform{
     id:number,
     name:string
     slug:string
 }
-const usePlatforms=()=>useData<Platform>("/platforms/lists/parents")
+const usePlatforms=()=>useQuery<FecthResponse<Platform>>({
+    queryKey:['plaforms'],
+    queryFn:()=> apiClient.get<FecthResponse<Platform>>('/platforms/lists/parents')
+    .then(res=>res.data)
+})
 export default usePlatforms
+/**
+ * usePlatforms=()=> useQuery({
+ *  queryKey:['platforms'],
+ *  queryFn: ()=> apiClient.get<fectResponse<Platform>>('/platforms/lists/parents').then(res=>res.data),
+ * staleTime:24*60*60*1000-24hrs,
+ * initialData:{count:platforms.length, result:platforms} // esto simula el contenido de la peticion de la api
+ * })
+ * 
+ * se debe de exportar la interface dek fectResponse del useData e importarlo aca para que no alla problema
+ * en que la data sea de tipo any
+ */
