2541affa5a187c5a204b6f8c5989e96062244848
Marcelo Garita <mgaritaf@outlook.com>
2024-04-11 18:17:01 -0600

    create access to home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInputGames from "./SearchInputGames";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="12px">
-      <Image src={logo} boxSize="60px" borderRadius={20} />
+      <Link to={"/"}>
+        <Image src={logo} boxSize="60px" borderRadius={20} />
+      </Link>
       <SearchInputGames />
       <ColorModeSwitch />
     </HStack>
